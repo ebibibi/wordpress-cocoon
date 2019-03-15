f53ce17df8b7cee16d25f5935dff1df709561151
Masahiko Ebisuda <ebibibi@gmail.com>
2019-03-15 14:12:40 +0900

    add $_SERVER['HTTPS'] = 'on'; to wp-config.php

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -70,6 +70,16 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 	$_SERVER['HTTPS'] = 'on';
 }
 EOPHP
+
+	cat << 'EOPHP' >> /usr/src/wordpress/wp-config.php
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+EOPHP
+
 	chown www-data:www-data wp-config.php
 fi
 
